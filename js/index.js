1d7fbc4a77f314c375b3c102d4c21a2fe2f09ce9
Oldrado Junior <oldradojunior@gmail.com>
2020-10-30 23:13:22 -0300

    Adiciona a exibição das alternativas na tela

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // Declaração de variáveis
 const answersBox = document.querySelector('#answers-box');
 const question = document.querySelector('#question');
+const letters = ['a', 'b', 'c', 'd'];
+let actualQuestion = 0;
 
 // Perguntas
 const questions = [
@@ -89,6 +91,37 @@ function createQuestion(i) {
 
     questionText.textContent = questions[i].question;
     questionNumber.textContent = i + 1;
+
+    // Insere as alternativas
+    questions[i].answers.forEach((answer, i) => {
+        // Cria o template do botão do Quizz
+        const answerTemplate = document
+            .querySelector('.answer-template')
+            .cloneNode(true);
+
+        const letterBtn = answerTemplate.querySelector('.btn-letter');
+        const answerText = answerTemplate.querySelector('.question-answer');
+
+        letterBtn.textContent = letters[i];
+        answerText.textContent = answer['answer'];
+
+        answerTemplate.setAttribute('correct-answer', answer['correct']);
+
+        // Remove hide e template class
+        answerTemplate.classList.remove('hide');
+        answerTemplate.classList.remove('answer-template');
+
+        // Insere a alternativa na tela
+        answersBox.appendChild(answerTemplate);
+
+        // Insere um evento de click no botão
+        answerTemplate.addEventListener('click', function () {
+            console.log(this);
+        });
+    });
+
+    // Incrementa o número da questão
+    actualQuestion++;
 }
 
 // Inicialização do Quizz
